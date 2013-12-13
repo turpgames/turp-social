796d1fc5630dd17364e61e45dff88071d2fd8640
mehmetatas <mehmetatasm@gmail.com>
2013-12-13 23:26:06 +0200

    facebook android sdk

--- turp-social-android/src/com/turpgames/ichigu/MainActivity.java
+++ turp-social-android/src/com/turpgames/ichigu/MainActivity.java
@@ -1,23 +1,63 @@
-package com.turpgames.social;
+package com.turpgames.ichigu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.turpgames.framework.v0.impl.android.AndroidProvider;
+import com.turpgames.framework.v0.impl.android.IAndroidLifecycleListener;
 import com.turpgames.framework.v0.impl.libgdx.GdxGame;
 import com.turpgames.framework.v0.util.Game;
 
 public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
-        
-        Game.setEnvironmentProvider(new AndroidProvider(this));
-                
-        initialize(new GdxGame(), cfg);
-    }
+	private IAndroidLifecycleListener lifecycleListener;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		cfg.useGL20 = false;
+
+		AndroidProvider prov = AndroidProvider.getInstance();
+		prov.init(this, savedInstanceState);
+		
+		Game.setEnvironmentProvider(prov);
+		
+		lifecycleListener = prov.getLifecycleListener();		
+		lifecycleListener.onCreate(savedInstanceState);
+		
+		initialize(new GdxGame(), cfg);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		lifecycleListener.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		lifecycleListener.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		lifecycleListener.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		lifecycleListener.onPause();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		lifecycleListener.onDestroy();
+	}
 }
--- turp-social/src/com/turpgames/social/SocialGame.java
+++ turp-social/src/com/turpgames/social/SocialGame.java
@@ -1,78 +1,105 @@
 package com.turpgames.social;
 
-import java.util.Date;
-
 import com.turpgames.framework.v0.forms.xml.Control;
 import com.turpgames.framework.v0.forms.xml.IControlActionHandler;
 import com.turpgames.framework.v0.forms.xml.IControlActionHandlerFactory;
 import com.turpgames.framework.v0.impl.BaseGame;
-import com.turpgames.framework.v0.social.ILoginCallback;
-import com.turpgames.framework.v0.social.IPostCallback;
+import com.turpgames.framework.v0.social.ICallback;
+import com.turpgames.framework.v0.social.ISocializer;
 import com.turpgames.framework.v0.social.Player;
+import com.turpgames.framework.v0.social.SocialFeed;
 import com.turpgames.framework.v0.util.Game;
 import com.turpgames.framework.v0.util.Utils;
 
 public class SocialGame extends BaseGame {
+	private ISocializer facebook;
+
 	@Override
 	public void init() {
 		Game.getResourceManager().beginLoading();
 		while (Game.getResourceManager().isLoading())
 			Utils.threadSleep(100);
 
-		Game.getActionHandlerFactory().setSuccessor(
-				new IControlActionHandlerFactory() {
-					@Override
-					public void setSuccessor(
-							IControlActionHandlerFactory successor) {
+		facebook = Game.getSocializer("facebook");
 
-					}
-
-					@Override
-					public IControlActionHandler create(Control control, String action) {
-						if ("login-facebook".equals(action)) {
-							return new IControlActionHandler() {
-								@Override
-								public void handle(Control control) {
-									Game.getSocializer("facebook").login(new ILoginCallback() {										
-										@Override
-										public void onLoginSuccess(Player player) {
-											System.out.println("social-id:  " + player.getSocialId());
-											System.out.println("email:      " + player.getEmail());
-											System.out.println("name:       " + player.getName());
-											System.out.println("avatar-url: " + player.getAvatarUrl());
-										}
-										
-										@Override
-										public void onLoginFail() {
-											
-										}
-									});
-								}
-							};
-						}
-						else if ("post-facebook".equals(action)) {
-							return new IControlActionHandler() {
-								@Override
-								public void handle(Control control) {
-									Game.getSocializer("facebook").postText("Sent from my Android: " + new Date(), new IPostCallback() {
-										
-										@Override
-										public void postSent() {
-											System.out.println("sent");
-										}
-										
-										@Override
-										public void postFailed() {
-											System.out.println("failed");
-										}
-									});
-								}
-							};
-						}
-						return IControlActionHandler.NULL;
-					}
-				});
+		Game.getActionHandlerFactory().setSuccessor(createSocailActionHandlerFactory());
 
 		super.init();
+	}
+
+	private IControlActionHandlerFactory createSocailActionHandlerFactory() {
+		return new IControlActionHandlerFactory() {
+			@Override
+			public void setSuccessor(IControlActionHandlerFactory successor) {
+			}
+
+			@Override
+			public IControlActionHandler create(Control control, String action) {
+				return createActionHandler(action);
+			}
+		};
+	}
+
+	private IControlActionHandler createActionHandler(String action) {
+		if ("login-facebook".equals(action)) {
+			return new IControlActionHandler() {
+				@Override
+				public void handle(Control control) {
+					facebookLogin();
+				}
+			};
+		}
+		else if ("post-facebook".equals(action)) {
+			return new IControlActionHandler() {
+				@Override
+				public void handle(Control control) {
+					facebookPostFeed();
+				}
+			};
+		}
+		return IControlActionHandler.NULL;
+	}
+
+	private void facebookLogin() {
+		facebook.login(new ICallback() {
+			@Override
+			public void onSuccess() {
+				Player player = facebook.getPlayer();
+				System.out.println("social-id:  " + player.getSocialId());
+				System.out.println("email:      " + player.getEmail());
+				System.out.println("name:       " + player.getName());
+				System.out.println("avatar-url: " + player.getAvatarUrl());
+			}
+
+			@Override
+			public void onFail(Throwable t) {
+				if (t != null)
+					t.printStackTrace();
+			}
+		});
+	}
+
+	private void facebookPostFeed() {
+		facebook.postFeed(
+				SocialFeed.newBuilder()
+						.setTitle("Title")
+						.setSubtitle("Subtitle")
+						.setHref("https://play.google.com/store/apps/details?id=com.turpgames.ichigu")
+						.setMessage("Message")
+						.setImageUrl("http://www.mbmt.net/homepage/github.png")
+						.build(),
+				new ICallback() {
+
+					@Override
+					public void onSuccess() {
+						System.out.println("sent");
+					}
+
+					@Override
+					public void onFail(Throwable t) {
+						if (t != null)
+							t.printStackTrace();
+					}
+				});
 	}
 }
